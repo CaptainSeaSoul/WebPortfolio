658630d0b4f32678d24a52eb1696e354bb938250
Ildar Ibiatov <34334432+CaptainGradus@users.noreply.github.com>
2021-02-09 14:39:52 +0300

    Mailing, sent notification, ticktick

--- WebPortfolio/Components/ContactForm.razor.cs
+++ WebPortfolio/Components/ContactForm.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,19 +15,57 @@ namespace WebPortfolio.Components
         private PortfolioContext Db { get; set; }
         [Inject]
         private IEmailService EmailService { get; set; }
+        [Inject]
+        private IJSRuntime JS { get; set; }
 
         private ContactFormModel contact = new ContactFormModel();
 
+        private Notification deliveryNotification = new Notification();
+
         private async void HandleValidSubmit()
         {
-            Db.Add(contact);
-            await Db.SaveChangesAsync();
-
             var message = $"Hi {contact.Name}!\n" +
                 $"Thank you for contacting me! I'll consider your request and reply as soon as I can." +
                 $"\nSincerely, Ildar";
 
-            await EmailService.SendAsync("Contact Request To Ildar", message, contact.Email, contact.Name);
+            try
+            {
+                await EmailService.SendAsync("Contact Request To Ildar", message, contact.Email, contact.Name);
+                deliveryNotification.SetSuccess();
+            }
+            catch (Exception e)
+            {
+                deliveryNotification.SetFailure(e.Message);
+            }
+
+            StateHasChanged();
+
+            await JS.InvokeVoidAsync("showNotification");
+
+            if (contact.Id == 0)
+            {
+                Db.Add(contact);
+                await Db.SaveChangesAsync();
+            }
+        }
+
+        class Notification
+        {
+            public bool Success { get; set; }
+            public String Message { get; set; }
+            public Boolean Show { get; private set; }
+            public void SetSuccess()
+            {
+                Success = true;
+                Message = "Thanks! Your request was sent successfully!";
+                Show = true;
+            }
+            public void SetFailure(String m)
+            {
+                Success = false;
+                Message = "Unfortunately, your message was not sent with error: " + m;
+                Show = true;
+            }
         }
     }
 }
--- WebPortfolio/wwwroot/js/site.js
+++ WebPortfolio/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+function showNotification() {
+    $('.toast').toast('show');
+}
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
